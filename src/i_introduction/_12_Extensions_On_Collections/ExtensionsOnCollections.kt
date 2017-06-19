ee1ff01e5924886fee4652228b854dc3e9b2297d
David Bliss <david.bliss@hivehome.com>
2017-06-19 10:40:31 +0100

    Extensions on collections

--- src/i_introduction/_12_Extensions_On_Collections/ExtensionsOnCollections.kt
+++ src/i_introduction/_12_Extensions_On_Collections/ExtensionsOnCollections.kt
@@ -2,9 +2,10 @@ package i_introduction._12_Extensions_On_Collections
 
 import util.TODO
 import util.doc12
+import java.util.*
 
 fun todoTask12(): Nothing = TODO(
-    """
+        """
         Task 12.
         In Kotlin standard library there are lots of extension functions that make the work with collections more convenient.
         Rewrite the previous example once more using an extension function 'sortedDescending'.
@@ -13,11 +14,16 @@ fun todoTask12(): Nothing = TODO(
         Thus in Kotlin we don't introduce our own collections, but use standard Java ones (slightly improved).
         Read about read-only and mutable views on Java collections.
     """,
-    documentation = doc12()
+        documentation = doc12()
 )
 
+fun MutableList<Int>.sortDescending(): Unit {
+    Collections.sort(this, { x, y -> y - x })
+}
+
 fun task12(): List<Int> {
-    todoTask12()
-    return arrayListOf(1, 5, 2)
+    val arrayList = arrayListOf(1, 5, 2)
+    arrayList.sortDescending()
+    return arrayList
 }
 
