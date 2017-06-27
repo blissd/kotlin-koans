30c7f21fdae7ec64843df1c7844a0d806335e3c5
David Bliss <david.bliss@hivehome.com>
2017-06-27 11:17:59 +0100

    iii.25) Comparison

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val y = year.compareTo(other.year)
+        val m = month.compareTo(other.month)
+        val d = dayOfMonth.compareTo(other.dayOfMonth)
+        if (y != 0) return y
+        if (m != 0) return m
+        return d
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
